9402ea5c4f7335c236703285de043b69fcb7a55c
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2022-12-02 22:30:07 -0500

    m

--- mona.py
+++ mona.py
@@ -8,13 +8,12 @@ dataset = pd.read_csv("refined_insurance.csv")
 dataset.head()
 
 age_smoker_bar = sns.barplot(x="BMI Category", y="age", hue="smoker", data=dataset, palette="Blues")
-plt.show
+
 
 smoker_age_box = sns.boxplot(x="smoker", y="age", data=dataset, palette="Blues")
-plt.show
+
 
 smoker_bmi_box = sns.boxplot(x="smoker", y="bmi", data=dataset, palette="Blues")
-plt.show
+
 
 smoker_expense_box = sns.boxplot(x="smoker", y="Total 2022 PPP expenses", data=dataset, palette="Blues")
-plt.show
