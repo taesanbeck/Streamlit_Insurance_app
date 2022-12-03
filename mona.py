1fff0010a06448dadf54fe6eb4e7e7ebf061978b
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2022-12-02 22:44:37 -0500

    m

--- mona.py
+++ mona.py
@@ -8,15 +8,23 @@ dataset = pd.read_csv("refined_insurance.csv")
 dataset.head()
 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(
+sns.barplot(
+    x="BMI Category", 
+    y="age", hue="smoker", 
+    data=dataset, palette="Blues"),
 
-sns.barplot(x="BMI Category", y="age", hue="smoker", data=dataset, palette="Blues")
+sns.boxplot(
+    x="smoker", 
+    y="age", data=dataset, palette="Blues"),
 
+sns.boxplot(
+    x="smoker", 
+    y="bmi", 
+    data=dataset, palette="Blues"),
 
-sns.boxplot(x="smoker", y="age", data=dataset, palette="Blues")
-
-
-sns.boxplot(x="smoker", y="bmi", data=dataset, palette="Blues")
-
-
-sns.boxplot(x="smoker", y="Total 2022 PPP expenses", data=dataset, palette="Blues")
+sns.boxplot(
+    x="smoker", 
+    y="Total 2022 PPP expenses", 
+    data=dataset, palette="Blues"),
+)
