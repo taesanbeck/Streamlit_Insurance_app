60baa0e3ae664d675143eaa1d90972c984daeef6
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2022-12-05 01:56:22 -0500

    Wide mode
    Added wide mode

--- app.py
+++ app.py
@@ -240,7 +240,12 @@ highests expenses (dark blue).''')
 st.altair_chart(km.cluster1)
 
 
-
+st.set_page_config(  # Alternate names: setup_page, page, layout
+	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+	page_title=None,  # String or None. Strings get appended with "• Streamlit". 
+	page_icon=None,  # String, anything supported by st.image, or None.
+)
 
 
 
