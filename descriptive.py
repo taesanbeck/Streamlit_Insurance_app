bfaec41b4ca0f589316cad9660eacd24f4f923d1
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2022-12-04 20:57:13 -0500

    m

--- descriptive.py
+++ descriptive.py
@@ -56,7 +56,7 @@ rule = alt.Chart(df).mark_rule(color='red').encode(
     y=alt.Y('Forward PPP expenses:Q',axis=alt.Axis(title=None))
 )
 
-(age_bar + rule).properties(width=500,height=300).configure_axis(
+age_bar_rule = (age_bar + rule).properties(width=500,height=300).configure_axis(
     grid=False, labelFontSize=20, titleFontSize = 20
 ).configure_legend(labelFontSize = 15,titleFontSize=15)
 ############################################################# Khalid's STUFF ##################
@@ -108,13 +108,13 @@ box_PPP_exp = alt.Chart(df).mark_boxplot().encode(
 
 #bar_smoker 
  # Kahlid's is better I needed pairs of 2
-b#ox_PPP_exp
+#box_PPP_exp
 
 ### Khalid's CHART's
 age_hist
-BMI_hist
-
 age_box
+
+BMI_hist
 bmi_box
 
-age_bar
+age_bar_rule
