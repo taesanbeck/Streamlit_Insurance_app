ac79134012ff03676c1103488fc3e766d314bf6a
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2022-12-04 23:18:25 -0500

    M

--- kmeans.py
+++ kmeans.py
@@ -41,7 +41,7 @@ cluster1 = cluster_age | cluster_bmi
 smoker_cluster2 = alt.Chart(df).mark_circle(size=60).encode(
     x='age',
     y='Total 2022 PPP expenses',
-    color= alt.Color("smoker:N", type="nominal",scale=alt.Scale(range=['#012bbf', ' #ac1008']),legend=alt.Legend(columns=2)),
+    color= alt.Color("smoker", type="nominal",scale=alt.Scale(range=['#012bbf', ' #ac1008']),legend=alt.Legend(columns=2)),
     tooltip=['age', 'bmi', 'region', 'smoker','Benefit from Proposed Price Yes/No','proposed_price','CLUSTERS']
 ).properties(width=500,height=500).interactive()
 
@@ -50,7 +50,7 @@ benifit_cluster2 = alt.Chart(df).mark_circle(size=60).encode(
     x='age',
     y='Total 2022 PPP expenses',
     color='Benefit from Proposed Price Yes/No',
-    color= alt.Color("Benefit from Proposed Price Yes/No:N", type="nominal",scale=alt.Scale(range=['#ac1008', '#00b10e']),legend=alt.Legend(columns=2)),
+    color= alt.Color("Benefit from Proposed Price Yes/No", type="nominal",scale=alt.Scale(range=['#ac1008', '#00b10e']),legend=alt.Legend(columns=2)),
     tooltip=['age', 'bmi', 'region', 'smoker','Benefit from Proposed Price Yes/No','proposed_price','CLUSTERS']
 ).properties(width=500,height=500).interactive()
 
