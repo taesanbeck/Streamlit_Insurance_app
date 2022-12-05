fc3f1d7c67b4f794817db0e55b9c22a22522126c
kalmasri97 <72473217+kalmasri97@users.noreply.github.com>
2022-12-04 19:31:21 -0500

    edit

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ st.markdown('''
 After conducting rigorous data analysis, our team looks to propose a business model that builds on that of a private company, GoForward, which provides preventative healthcare services, with enhanced pricing that tailors to individual nuances
 and ensures all parties involved stand to benefit - private businesses, government, and, most importantly, our people. Our solution primarily aims to further disintermediate preventative healthcare services by removing insurance companies from the equation.
 
-The GoForward model is predicated on the buildout of establishments that have all the resources necessary to provide preventative healthcare services. With a monthly subscription model, patients may access those facilities, and all services provided, at any time and at no additional cost.
+The GoForward model is predicated on the buildout of establishments that have all the resources necessary to provide preventative, prescription, and primary care healthcare services. With a monthly subscription model, patients may access those facilities, and all services provided, at any time and at no additional cost.
 Currently, GoForward uses a universal pricing system at $150/month. However, we believe this does not accurately capture distinctions across age and overall health levels. We propose a pricing model that is enhanced such that those nuances are accommodated. 
  
 ''')
